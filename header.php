641625775027ea421fc98225f2718c0b15dec1bd
Matthew Murphy <mashumafi@gmail.com>
2014-03-14 14:56:11 -0400

    removed main.css from project, preferred wp default css file

--- header.php
+++ header.php
@@ -18,7 +18,6 @@
 
     <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/css/bootstrap.min.css">
     <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/css/bootstrap-theme.min.css">
-    <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/css/main.css">
     <link rel="stylesheet" href="<?php echo get_stylesheet_uri(); ?>" type="text/css" media="screen" />
 
     <script src="<?php echo get_template_directory_uri(); ?>/js/vendor/modernizr-2.6.2-respond-1.1.0.min.js"></script>
